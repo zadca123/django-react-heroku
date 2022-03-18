2fd5b961798a7e63ae9642b5cc34bfd3797f0cea
zadca123 <zadca123@debian-BULLSEYE-live-builder-AMD64>
2022-03-18 09:38:17 +0100

    changing value from "localhost" to "127.0.0.1" in DATABASES array

--- backend/settings.py
+++ backend/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'procode',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'USER': 'root',
         'PASSWORD': '',
         'OPTIONS': {
